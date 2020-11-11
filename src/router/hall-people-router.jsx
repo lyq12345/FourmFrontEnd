9c7f8b7154928dac01dc88b4e9b52e1f8dd2c132
xnwang02 <xnwang02@mail.nfsq.com.cn>
2020-11-11 17:20:49 +0800

    fix:路由标题添加

--- src/router/hall-people-router.jsx
+++ src/router/hall-people-router.jsx
@@ -2,39 +2,39 @@ module.exports = {
   router: [
     {
       path: '/hall-people',
-      name: 'hall-people',
+      name: '每周堂里人',
       routes: [
         {
           path: '/hall-people',
-          name: 'hall-people',
+          name: '每周堂里人',
           component: './HallPeople',
         },
         {
           path: '/hall-people/detail',
-          name: 'detail',
+          name: '每周堂里人详情',
           component: './HallPeople/Detail',
         },
       ]
     },
     {
       path: '/hall-something',
-      name: 'hall-something',
+      name: '堂里新鲜事',
       routes: [
         {
           path: '/hall-something',
-          name: 'hall-something',
+          name: '堂里新鲜事',
           component: './HallSomething',
         },
         {
           path: '/hall-something/detail',
-          name: 'detail',
+          name: '堂里新鲜事详情',
           component: './HallSomething/Detail',
         },
       ]
     },
     {
       path: '/personal-homepage',
-      name: 'personal-homepage',
+      name: '个人主页',
       component: './PersonalHomepage',
     },
   ]
