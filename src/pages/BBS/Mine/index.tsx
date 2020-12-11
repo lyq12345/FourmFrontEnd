021ca275c4601b7e34748c47bbaeb5a8830a0e8e
hzhong06 <hzhong06@mail.yst.com.cn>
2020-12-11 15:44:57 +0800

    ✨feat(BBS): 广场列表帖子数、我发的帖子

--- src/pages/BBS/Mine/index.tsx
+++ src/pages/BBS/Mine/index.tsx
@@ -3,5 +3,20 @@ import { requestMyPosts } from '../api';
 import NormalPostList from '../components/NormalPostList';
 
 export default React.memo(() => {
-  return <NormalPostList requestFn={requestMyPosts} />;
+  return (
+    <>
+      <p
+        style={{
+          fontSize: 16,
+          lineHeight: 1,
+          margin: '12px 0',
+          fontWeight: 500,
+          color: '#333',
+        }}
+      >
+        我发的帖子
+      </p>
+      <NormalPostList requestFn={requestMyPosts} />
+    </>
+  );
 });
