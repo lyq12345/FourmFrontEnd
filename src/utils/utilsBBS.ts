2133970f4a1ef91ba58e33d55c65f8968658039e
hzhong06 <hzhong06@mail.yst.com.cn>
2020-12-22 16:36:15 +0800

    ✨feat(BBS): 定制Relative Time

--- src/utils/utilsBBS.ts
+++ src/utils/utilsBBS.ts
@@ -49,6 +49,7 @@ var relativeTime = require('dayjs/plugin/relativeTime');
 var config = {
   thresholds: [
     { l: 's', r: 1 },
+    { l: 'ss', r: 59, d: 'second' },
     { l: 'm', r: 1 },
     { l: 'mm', r: 59, d: 'minute' },
     { l: 'h', r: 1 },
@@ -63,6 +64,26 @@ var config = {
 };
 require('dayjs/locale/zh-cn');
 dayjs.extend(relativeTime, config).locale('zh-cn');
+var updateLocale = require('dayjs/plugin/updateLocale');
+dayjs.extend(updateLocale);
+dayjs.updateLocale('zh-cn', {
+  relativeTime: {
+    s: () => '刚刚',
+    ss: () => '刚刚',
+    future: 'in %s',
+    past: '%s',
+    m: '1分钟前',
+    mm: '%d分钟前',
+    h: '1小时前',
+    hh: '%d小时前',
+    d: '1天前',
+    dd: '%d天前',
+    M: '1个月前',
+    MM: '%d个月前',
+    y: '1年前',
+    yy: '%d年前',
+  },
+});
 export { dayjs };
 
 // 500 ms 的防抖
